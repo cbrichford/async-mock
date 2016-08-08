86f305578ce3571a806ae94296e8e32c9049a8d9
Christopher Brichford <chrisb+git@farmersbusinessnetwork.com>
2016-08-08 16:54:49 -0700

    Adding waitForSingleCall utility method.

--- async_mock/coroutine.py
+++ async_mock/coroutine.py
@@ -7,15 +7,23 @@ def SimpleCoroutineMock(f=lambda *args, **kwargs: None, loop=None):
 
 
 class CoroutineMock(object):
+
+    # Handy for debugging failing tests in the debugger.
+    __blocking_dict = {}
+
     def __init__(self, loop, returnSequence, block:asyncio.Event):
-        self.__start = asyncio.Event(loop=loop)
-        self.__end = asyncio.Event(loop=loop)
+        self.__startingEvent = asyncio.Event(loop=loop)
+        self.__endingEvent = asyncio.Event(loop=loop)
         self.__returnSequence = tuple(returnSequence)
         if (len(self.__returnSequence) < 1):
             self.__returnSequence = (lambda *args, **kwargs: None, )
         self.__returnSequenceLen = len(self.__returnSequence)
         self.__block = block
         self.__mock = self.__createMock()
+        # It's easier to find a dictionary that is an instance variable than
+        # one that is a class static, so just make an instance variable that
+        # references the shared dictionary.
+        self.__blocking_dict = CoroutineMock.__blocking_dict
 
     def __createMock(self):
         returnIndex = 0
@@ -23,31 +31,40 @@ class CoroutineMock(object):
         async def cr(*args, **kwargs):
             nonlocal returnIndex
             try:
-                self.__end.clear()
-                self.__start.set()
+                self.__endingEvent.clear()
+                self.__startingEvent.set()
                 if (self.__block is not None):
-                    await self.__block.wait()
+                    self.__blocking_dict[id(self)] = self
+                    try:
+                        await self.__block.wait()
+                    finally:
+                        del self.__blocking_dict[id(self)]
                     self.__block.clear()
                 returnFunc = self.__returnSequence[returnIndex % self.__returnSequenceLen]
                 returnIndex += 1
                 return returnFunc(*args, **kwargs)
             finally:
-                self.__start.clear()
-                self.__end.set()
+                self.__startingEvent.clear()
+                self.__endingEvent.set()
 
         return MagicMock(wraps=cr)
 
     def start(self):
-        return self.__start
+        return self.__startingEvent
 
     def end(self):
-        return self.__end
+        return self.__endingEvent
 
     def unblock(self):
         self.__block.set()
 
     def mock(self):
         return self.__mock
+
+    async def waitForSingleCall(self):
+        await self.start().wait()
+        self.unblock()
+        await self.end().wait()
 
 
 class CoroutineMockBuilder(object):
